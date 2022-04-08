68b0a63276d138b047d7515815197eb4633699bb
Sakis Koltsidas <sakis.koltsidas@exizent.com>
2022-04-08 22:49:15 +0100

    Refactor to extract extra definition delimiter and apply them if available

--- src/Isometrix/StringCalculator.cs
+++ src/Isometrix/StringCalculator.cs
@@ -1,24 +1,43 @@
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 
 namespace Isometrix;
 
 public class StringCalculator
 {
+    private static readonly ImmutableList<char> Delimiters = new[] {',', '\n'}.ToImmutableList();
+
     public int Add(string numbers)
     {
         if (numbers is not {Length: > 0})
             return 0;
 
-        var delimiters = new List<char>(new[] {',', '\n'});
-        
-        if (numbers.StartsWith("//"))
+        if (TryGetExtraDelimiter(numbers, out var extraDelimiter))
         {
-            delimiters.Add(numbers[2]);
-            var firstNewLine = numbers.IndexOf('\n');
-            numbers = numbers.Substring(firstNewLine + 1);
+            numbers = RemoveDelimiterDefinition(numbers);
         }
 
-        return numbers.Split(delimiters.ToArray()).Sum(int.Parse);
+        return numbers.Split(Delimiters.Add(extraDelimiter).ToArray()).Sum(int.Parse);
+    }
+
+    private static bool HasDelimiterDefinition(string numbers) => numbers.StartsWith("//");
+    
+    private static bool TryGetExtraDelimiter(string numbers, out char extraDelimiter)
+    {
+        if (HasDelimiterDefinition(numbers))
+        {
+            extraDelimiter = numbers[2];
+            return true;
+        }
+
+        extraDelimiter = default;
+        return false;
+    }
+
+    private static string RemoveDelimiterDefinition(string numbers)
+    {
+        var firstNewLine = numbers.IndexOf('\n');
+        return numbers[(firstNewLine + 1)..];
     }
 }
