e8c8e9039b0c2bd22c32534b67725439d694bb8e
Sakis Koltsidas <sakis.koltsidas@exizent.com>
2022-04-08 20:01:48 +0100

    Support adding a single number and returning the number

--- src/Isometrix/StringCalculator.cs
+++ src/Isometrix/StringCalculator.cs
@@ -2,5 +2,11 @@ namespace Isometrix;
 
 public class StringCalculator
 {
-    public int Add(string numbers) => 0;
+    public int Add(string numbers)
+    {
+        if (numbers is {Length: > 0})
+            return int.Parse(numbers);
+        
+        return 0;
+    }
 }
