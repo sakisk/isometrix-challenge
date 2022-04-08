0c8d49b0ac689c3b6afcb18cdfcde3080ecfd1c5
Sakis Koltsidas <sakis.koltsidas@exizent.com>
2022-04-08 20:33:42 +0100

    Support adding numbers delimited with mixed delimiters of new lines or commas

--- src/Isometrix/StringCalculator.cs
+++ src/Isometrix/StringCalculator.cs
@@ -7,5 +7,5 @@ public class StringCalculator
     public int Add(string numbers) => 
         numbers is not {Length: > 0} 
             ? 0 
-            : numbers.Split(',').Sum(int.Parse);
+            : numbers.Split(',', '\n').Sum(int.Parse);
 }
