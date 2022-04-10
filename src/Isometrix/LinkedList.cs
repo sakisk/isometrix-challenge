565a619674f4c3e664e3c1f017a76aeac3f285d5
Sakis Koltsidas <sakis.koltsidas@exizent.com>
2022-04-10 14:49:54 +0100

    Test that a first node can be added many times setting the old one to be next

--- src/Isometrix/LinkedList.cs
+++ src/Isometrix/LinkedList.cs
@@ -1,12 +1,14 @@
+using System;
+
 namespace Isometrix;
 
 public class LinkedList<T>
 {
     public LinkedListNode<T>? First { get; private set; }
 
-    public void AddFirst(T newNode)
+    public void AddFirst(T element)
     {
-        var newFirstNode = new LinkedListNode<T>(newNode)
+        var newFirstNode = new LinkedListNode<T>(element)
         {
             Next = First
         };
--- tests/IsometrixTests/LinkedListTests.cs
+++ tests/IsometrixTests/LinkedListTests.cs
@@ -1,3 +1,4 @@
+using System;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using Isometrix;
@@ -43,6 +44,25 @@ public class LinkedListTests
         using var _ = new AssertionScope();
         sut.First!.Data.Should().Be(newFirstNode);
         sut.First!.Next!.Data.Should().Be(old);
+    }
+
+    [Fact]
+    public void ShouldAllowAddingNewFirstNodeManyTimes()
+    {
+        const int first = 1;
+        const int second = 2;
+        const int third = 3;
+        
+        var sut = new LinkedList<int>();
+        
+        sut.AddFirst(first);
+        sut.AddFirst(second);
+        sut.AddFirst(third);
+
+        using var _ = new AssertionScope();
+        sut.First!.Data.Should().Be(third);
+        sut.First!.Next!.Data.Should().Be(second);
+        sut.First!.Next!.Next!.Data.Should().Be(first);
     }
 
     private class TestType
